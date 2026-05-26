dfdd4075afe6c13605295cd3e717bdac6c9300bc
Richard Darnell <rd-github@rdnet.co.uk>
2026-05-26 22:30:43 +0100

    Fix silly cut and paste error

--- custom_components/wokingham_waste_collection/config_flow.py
+++ custom_components/wokingham_waste_collection/config_flow.py
@@ -5,7 +5,7 @@ import voluptuous as vol
 from homeassistant import config_entries
 from homeassistant.core import callback
 
-DOMAIN = "DOMAIN = "wokingham_waste_collection"
+DOMAIN = "wokingham_waste_collection"
 
 _LOGGER = logging.getLogger(__name__)
 
